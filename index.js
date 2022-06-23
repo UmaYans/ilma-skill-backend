28517958b9b94d7d679bf2f0c9c98fa7d5548e9d
керимов алихан <yakhya2809@gmail.com>
2022-06-23 13:19:54 +0300

    Минимальный бэк

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(require("./routes"));
+
+mongoose
+  .connect(process.env.MONGO_SERVER)
+  .then(() => console.log("Успешно соединились с сервером MongoDB"))
+  .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`hello port http://localhost:${process.env.PORT}`);
+});
