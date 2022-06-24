39f6e29bd762b56c119db4dc51df145d9f57b198
керимов алихан <yakhya2809@gmail.com>
2022-06-24 11:39:47 +0300

    Добавил текст в модель категорий

--- models/Category.model.js
+++ models/Category.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema({
   name: String,
+  text: String
 });
 
 const Category = mongoose.model("Category", categorySchema);
