8045bf9da697e688228cf66da5ca82eafaea1aad
керимов алихан <yakhya2809@gmail.com>
2022-06-27 03:30:17 +0300

    log

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.use(require("./categories.route.js"));
-// router.use(require("./comments.route.js"));
+router.use(require("./comments.route.js"));
 router.use(require("./services.route.js"));
 router.use(require("./users.route"));
 
